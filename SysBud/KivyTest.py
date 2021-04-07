35bb03c27b0ab2426dc3326b28362afb35969890
AxelNFritz <axelf.skype@gmail.com>
2021-04-08 00:08:36 +0200

    more GUI

--- SysBud/KivyTest.py
+++ SysBud/KivyTest.py
@@ -7,23 +7,19 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
- 
-
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
 
 class MyGridLayout(GridLayout):
 
     def __init__(self, **kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
-
         self.cols = 1
-        self.row_force_default=True
-        self.row_default_height=120
 
         self.first = SearchElement()
 
-        self.addsear = Button(text="+ Add search element +", font_size=26)
+        self.addsear = Button(text="+ Add search element +", font_size=26, size_hint_y = None, height = 30)        
         self.addsear.bind(on_press=self.addbutt)
-
         self.add_widget(self.addsear)
         self.add_widget(self.first.element)
 
@@ -41,9 +37,10 @@ class MyApp(App):
 class SearchElement(GridLayout):
     def __init__(self):
 
-        self.element = GridLayout(cols = 1, row_force_default=True, row_default_height=30)
-
-        self.top_grid = GridLayout(cols = 3)
+        self.element = GridLayout(cols = 1, size_hint_y = None, height = 180)
+        self.top_grid = GridLayout(cols = 3, size_hint_y = None, height = 30)
+        self.scroll = ScrollView(do_scroll_x = False, size_hint=(1, None), size=(Window.width, 150))
+        self.scrollgrid = GridLayout(cols = 1, row_force_default=True, row_default_height=30, size_hint_x = 1, size_hint_y = None, height = 30000)
 
         self.top_grid.add_widget(Label(text="search: "))
         self.search = TextInput(multiline=False)
@@ -53,14 +50,17 @@ class SearchElement(GridLayout):
         self.submit.bind(on_press=self.press)
         self.top_grid.add_widget(self.submit)
 
+        self.scroll.add_widget(self.scrollgrid)
         self.element.add_widget(self.top_grid)
+        self.element.add_widget(self.scroll)
 
     def press(self, instance):
         sea = self.search.text
         if sea != "":
             res = SysSortiment.apk_search("namn1", sea)
             for row in res:
-                self.element.add_widget(Label(text=f"{row}"))
+                self.scrollgrid.add_widget(Label(text=f"{row}"))
+
 
 if __name__ == '__main__':
     MyApp().run()
