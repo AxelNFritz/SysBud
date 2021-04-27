93c718bfb9fb07c16e44337115d0a2b2a8e6cfd6
AxelNFritz <axelf.skype@gmail.com>
2021-04-27 12:32:53 +0200

    Spinners and Sorting progress

--- SysBud/SysSortiment.py
+++ SysBud/SysSortiment.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 
+
 exel = 'Sortiment6.xlsx'
 db = 'sysdb6.db'
 
@@ -18,15 +19,15 @@ def create_db(db_file):
     print("Done!")
 
 def generate_db(exel_file, cur):
-    df = pd.read_excel(exel_file)
+    df = pd.read_excel(exel_file) #Slowing it all down?
     for row in df.iterrows():
         r = row[1].tolist()
-        colums = [r[0], r[3], r[4], r[5], r[7], r[8], r[11], r[12], r[13], r[17], r[18], r[22]] #[0:3:4:5:7:8:11:12:13:17:18:22]
+        colums = [r[0], r[3], r[4], r[5], r[7], r[8], r[11], r[12], r[13], r[17], r[18], r[22][:-1]] #[0:3:4:5:7:8:11:12:13:17:18:(r[22][:-1])]
         add_bev(colums, cur)
 
 def add_bev(row, cur):
     statement=f"INSERT INTO dryck_utbud values('{row[0]}', '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}', '{row[5]}', '{row[6]}', '{row[7]}', '{row[8]}', '{row[9]}', '{row[10]}', '{row[11]}');"
-    cur.execute("begin transaction;")
+    cur.execute("begin transaction;") #Move tansaction to generate_db when it all works
     try:
         cur.execute(statement)
         cur.execute("commit;")
@@ -34,24 +35,37 @@ def add_bev(row, cur):
         cur.execute("rollback;")
         print(row), print(" Was rejected")
 
-def search(column, sear):
+
+
+def search(column, sear, land, varugrupp):
     con, cur = get_connection(db)
-    statement=f"SELECT * FROM dryck_utbud WHERE {column} LIKE '%{sear}%';"
+    land_state=f"AND land='{land}'"
+    varugrupp_state=f"AND varugrupp='{varugrupp}'"
+    if land == 'Land': land_state = ""
+    if varugrupp == 'Varugrupp': varugrupp_state = ""
+    statement=f"SELECT * FROM dryck_utbud WHERE {column} LIKE '%{sear}%' {land_state} {varugrupp_state};"
     cur.execute(statement)
-    #cur.execute("SELECT * FROM dryck_utbud WHERE producent like '%Fuller%';")
-    res = cur.fetchall() #[(list0), (list1), ...] tuples!? ;(
+    res = cur.fetchall() #[(tupel0), (tupel1), ...]
     con.close()
     #df = pd.DataFrame(res)
     return res
 
-def apk_sort(lis):
+def top_search(n, sort, land, typ):
+    con, cur = get_connection(db)
+    statement_ = f"SELECT * FROM dryck_utbud WHERE {column}"
+
+
+    con.close()
+    return res
+
+def apk_add(lis):
     res = []
     for row in lis:
         p = row[11]
         pe = float(p[:-1])
         krl = row[5]
         if pe != 0:
-            apk = (pe/krl)
+            apk = (pe/krl)*100
         else: 
             apk = 0
         lrow = list(row)
@@ -60,9 +74,23 @@ def apk_sort(lis):
 
     return res
 
-def apk_search(column, sear):
-    res = apk_sort(search(column, sear))
+def apk_sort(lis):
+    res = apk_add(lis)
+    res = sorted(res, key=lambda x: float(x[12]))[::-1]
+    return res    
+
+def krl_sort(lis):
+    res = apk_add(lis)
+    res = sorted(res, key=lambda x: float(x[5][:-1]))  
+    return res  
+
+def apk_search(column, sear, land, varugrupp):
+    res = apk_sort(search(column, sear, land, varugrupp))
     return res
+
+def reg_search(column, sear, land, varugrupp):
+    res = apk_add(search(column, sear, land, varugrupp))
+    return res    
 
 def get_spinner_ops():
     con, cur = get_connection(db)
@@ -72,9 +100,11 @@ def get_spinner_ops():
     rows1 = cur.fetchall()
     cur.execute(statement2)
     rows2 = cur.fetchall()
+
     varugrupp_list = list()
     for row in rows1:
         varugrupp_list.append(row[0])
+
     land_list = list()
     for row in rows2:
         land_list.append(row[0])
